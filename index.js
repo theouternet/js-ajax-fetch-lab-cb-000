d847e289dccbf2aa6338286758e858778e67fe63
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-29 21:29:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,6 +39,24 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  
+   const repo = `${user}/js-ajax-fetch-lab`;
+    const url = `${baseURL}/repos/${repo}/issues`;
+    const postData = {
+      title: document.getElementById('title').value,
+      body: document.getElementById('body').value
+    };
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+      .then(res => res.json())
+      .then(json => getIssues());
+  
 }
 
 function getIssues() {
